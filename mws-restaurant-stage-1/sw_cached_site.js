3c75f72d7350423d4a99d16e671f5b5a71e5a279
Musaab Abdalla <amazonfiretv.test@gmail.com>
2018-11-30 10:48:55 -0500

    fix: a bug fix

--- mws-restaurant-stage-1/sw_cached_site.js
+++ mws-restaurant-stage-1/sw_cached_site.js
@@ -1,26 +1,26 @@
 const CACHE_VERSION = 'restaurant-v2';
 const FILESTOCACHE = [
-  '/index.html',
   './', // alais to index.html
-  '/restaurant.html',
-  '/css/styles.css',
-  '/js/dbhelper.js',
-  '/js/main.js',
-  '/js/restaurant_info.js',
-  '/data/restaurants.json',
-  '/img/1.jpg',
-  '/img/2.jpg',
-  '/img/3.jpg',
-  '/img/4.jpg',
-  '/img/5.jpg',
-  '/img/6.jpg',
-  '/img/7.jpg',
-  '/img/8.jpg',
-  '/img/9.jpg',
-  '/img/10.jpg',
-  '//unpkg.com/leaflet@1.3.1/dist/leaflet.js',
+  'index.html',
+  'restaurant.html',
+  'css/styles.css',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js',
+  'data/restaurants.json',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
   // '//normalize-css.googlecode.com/svn/trunk/normalize.css',
-  '//unpkg.com/leaflet@1.3.1/dist/leaflet.css'
+  'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
 ];
 
 // Install ServiceWorker
