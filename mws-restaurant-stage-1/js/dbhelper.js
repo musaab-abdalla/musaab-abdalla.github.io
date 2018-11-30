7060b1668da6fd40e49d30a3075bbe6561435ef9
Musaab Abdalla <amazonfiretv.test@gmail.com>
2018-11-29 22:53:03 -0500

    fix: add database url hostname

--- mws-restaurant-stage-1/js/dbhelper.js
+++ mws-restaurant-stage-1/js/dbhelper.js
@@ -9,6 +9,11 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 3000 // Change this to your server port
+
+    if(isHosted){
+    	console.log(window.location.hostname);
+      return `https://musaab-abdalla.github.io/mws-restaurant-stage-1/data/restaurants.json`;
+    }
     return `http://localhost:${port}/data/restaurants.json`;
   }
 
