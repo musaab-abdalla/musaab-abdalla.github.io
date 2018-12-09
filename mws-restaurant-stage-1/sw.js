0e5f173dfa73137b976dd10860db6cd81c524b3e
Musaab Abdalla <amazonfiretv.test@gmail.com>
2018-12-08 21:52:37 -0500

    refactor: refactoring production code

--- mws-restaurant-stage-1/sw.js
+++ mws-restaurant-stage-1/sw.js
@@ -1,36 +1,38 @@
 // Names of the cache used in this version of the service worker
-const staticCacheName = 'restaurant-v3';
+const staticCacheName = 'restaurant-v1';
 
-// A list of resources always want to be cached
-const urlToCache = [
-  '/mws-restaurant-stage-1/',
-  '/mws-restaurant-stage-1/index.html',
-  '/mws-restaurant-stage-1/css/styles.css',
-  '/mws-restaurant-stage-1/js/dbhelper.js',
-  '/mws-restaurant-stage-1/js/main.js',
-  '/mws-restaurant-stage-1/js/restaurant_info.js',
-  '/mws-restaurant-stage-1/js/indexController.js',
-  '/mws-restaurant-stage-1/data/restaurants.json',
-  '/mws-restaurant-stage-1/img/1.jpg',
-  '/mws-restaurant-stage-1/img/2.jpg',
-  '/mws-restaurant-stage-1/img/3.jpg',
-  '/mws-restaurant-stage-1/img/4.jpg',
-  '/mws-restaurant-stage-1/img/5.jpg',
-  '/mws-restaurant-stage-1/img/6.jpg',
-  '/mws-restaurant-stage-1/img/7.jpg',
-  '/mws-restaurant-stage-1/img/8.jpg',
-  '/mws-restaurant-stage-1/img/9.jpg',
-  '/mws-restaurant-stage-1/img/10.jpg',
+// A list of assets always want to be cached
+const filesTocache = [
+  '/',
+  'index.html',
+  'css/styles.css',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js',
+  'js/indexController.js',
+  'data/restaurants.json',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
   'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
   'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css'
 ];
 
-// The install handler takes care of precaching the resources we always need
+// The install handler takes care of precaching the assets we always need
 self.addEventListener('install', event => {
   event.waitUntil(
+    // Open a cache....
     caches.open(staticCacheName)
-      .then(cache => cache.addAll(urlToCache)
-      )
+      // Add assets to it
+      .then(cache => cache.addAll(filesTocache))
+      // Skip the 'waiting' lifecycle phase, to go directly from 'installed' to 'activated'
       .then(() => self.skipWaiting())
   );
 });
@@ -41,33 +43,45 @@ self.addEventListener('activate', event => {
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheName != staticCacheName) {
+          if (staticCacheName.indexOf(cacheName) === -1) {
+            // If this cache name isn't present in the array of "expected" cache names, then delete it.
             return caches.delete(cacheName);
           }
         })
-      )
-    })
+      );
+      // Tell the active service worker to take immediate control of all of the clients under its scope
+    }).then(() => self.clients.claim())
   );
 });
 
-// The fetch handler serves responses for resources from a cache
+// The fetch handler serves responses for assets from a cache
 self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
   event.respondWith(
+    // We look for something in the caches that matches the request
     caches.match(event.request)
-      .then((response) => {
-        return response || caches.open(staticCacheName).then(cache => {
-          return fetch(event.request).then((response) => {
-            if (response.status === 404) {
-              return new Response('Page not found.')
-            }
-            if (event.request.url.indexOf('restaurant.html') != -1 || event.request.url.indexOf('leaflet') != -1) {
-              cache.put(event.request, response.clone());
-            }
-            return response;
+      .then(response => {
+        // If we get something, we return it || response may be used only once
+        if (response) {
+          return response;
+        }
+        // We'll pass the request to fetch, which will use the network.
+        return fetch(event.request)
+          .then(response => {
+            // TODO 5 - Respond with custom 404 page
+            return caches.open(staticCacheName).then(cache => {
+              if (event.request.url.indexOf('restaurant.html') !== -1 || event.request.url.indexOf('leaflet') !== -1) {
+                // We need to save clone to put one copy in cache and serve second one
+                cache.put(event.request.url, response.clone());
+              }
+              return response;
+            });
           });
-        });
-      }).catch(() => new Response('<p>You seems to be offline, and we didn\'t find any old cache for the URL.</p>', {
-        headers: { 'Content-Type': 'text/html' }
-      }))
+      }).catch(() => {
+        // Cache or fallback
+        return new Response(`<p>Failed to display contents from cache storage: ${event.request.url}</p>`,
+          { headers: { 'Content-Type': 'text/html' } }
+        );
+      })
   );
 });
