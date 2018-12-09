edcf7ee6a0121b1c71fb4ee6c585d8efd24c812b
Musaab Abdalla <amazonfiretv.test@gmail.com>
2018-12-08 21:49:08 -0500

    style: change comments to multiple lines format

--- mws-restaurant-stage-1/js/indexController.js
+++ mws-restaurant-stage-1/js/indexController.js
@@ -1,12 +1,14 @@
-// Register service worker
-// if the service worker API is available, and it is registered once the page is loaded.
+/**
+ * Register service worker
+ * if the service worker API is available, and it is registered once the page is loaded.
+*/
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker
-            .register('/mws-restaurant-stage-1/sw.js', { scope: '/mws-restaurant-stage-1/'})
-            // Registration was successful
-            .then(reg => console.log(`ServiceWorker registration successful with scope: ${reg.scope}.`))
-            // registration failed
-            .catch(err => console.log(`ServiceWorker registration failed: ${err}.`))
+      navigator.serviceWorker
+        .register('/sw.js')
+        // Registration was successful
+        .then(reg => console.log(`ServiceWorker registration successful with scope: ${reg.scope}.`))
+        // registration failed
+        .catch(err => console.log(`ServiceWorker registration failed: ${err}.`))
     })
-}
+  }
