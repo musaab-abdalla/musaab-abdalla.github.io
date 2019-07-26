e8dfc38f96b8d4c9c36ab409159b21bade56dc68
Musaab Abdalla <amazonfiretv.test@gmail.com>
2019-07-25 23:31:43 -0400

    refactor: Refactoring player, enemy collision and key press movements.

--- frontend-nanodegree-arcade-game/js/app.js
+++ frontend-nanodegree-arcade-game/js/app.js
@@ -1,95 +1,86 @@
-// Global variable.
-var poY = 10;
-var enemySpeed = 300;
 // Enemies constructor
 var Enemy = function(x, y) {
-    // Variables applied to each of our instances
-    this.x = x;
-    this.y = y;
-    this.speed = Math.floor(200 + (Math.random() * enemySpeed));
-    // The image/sprite for our enemies
-    this.sprite = 'images/enemy-bug.png';
+  // Variables applied to each of our instances
+  this.x = x;
+  this.y = y;
+  // The image/sprite for our enemies
+  this.sprite = 'images/enemy-bug.png';
+  this.height = 65;
+  this.width = 72;
 };
 
 // Update the enemy's position
 Enemy.prototype.update = function(dt) {
-    // Ensure enemy bugs move across screen.
-    this.x = this.x + (this.speed * dt);
-    if (this.x > 550) {
-        this.x = -100;
-        this.speed = Math.floor(200 + (Math.random() * enemySpeed));
-        if (this.y > 226) {
-            this.y = 60;
-        }
-    }
-    // Check for collision.
-    if (player.y >= this.y - poY && player.y <= this.y + poY) {
-        if (player.x >= this.x - poY && player.x <= this.x + poY) {
-            alert('YOU LOSE!, TRY AGAIN');
-            player.resetGame();
-        }
-    }
+  // Ensure enemy bugs move across screen.
+  if (this.x > ctx.canvas.width + this.width) {
+    this.x = -200 * Math.floor(Math.random() * 4) + 1;
+  } else {
+    this.x += 400 * dt;
+  }
+  // Check for collision.
+  if (
+    player.x < this.x + this.width &&
+    player.x + player.width > this.x &&
+    player.y < this.y + this.height &&
+    player.height + player.y > this.y
+  ) {
+    alert('YOU LOSE!, TRY AGAIN');
+    player.resetGame();
+  }
 };
 
 // Draw the enemy on the screen.
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Player constructor.
-var Player = function() {
-    this.x = 202;
-    this.y = 404;
-    this.sprite = 'images/char-boy.png';
+var Player = function(x, y) {
+  this.x = x;
+  this.y = y;
+  this.sprite = 'images/char-boy.png';
+  this.height = 75;
+  this.width = 65;
 };
 
-// Arrow keys to move the player
+// When player reach the water alert the player and reset the game.
 Player.prototype.update = function() {
-    if (this.keypress === 'left') {
-        this.x -= 101;
-        // keep player move inside the canvas
-        if (this.x <= 0) {
-            this.x = 0;
-        }
-    }
-    if (this.keypress === 'up') {
-        this.y -= 85;
-    }
-    if (this.keypress === 'right') {
-        this.x += 101;
-        // keep player move inside the canvas
-        if (this.x >= 404) {
-            this.x = 404;
-        }
-    }
-    if (this.keypress === 'down') {
-        this.y += 85;
-        // keep player move inside the canvas
-        if (this.y >= 404) {
-            this.y = 404;
-        }
-    }
-    this.keypress = null;
-    if (this.y < 60) {
-        alert('YOU WIN, CONGRATULATIONS!');
-        this.resetGame();
-    }
+  if (player.y < 0) {
+    alert('YOU WIN, CONGRATULATIONS!');
+    this.resetGame();
+  }
 };
 
 // Draw the player on the screen.
 Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Set the keys for moving the player.
-Player.prototype.handleInput = function(key) {
-    this.keypress = key;
+Player.prototype.handleInput = function(keypress) {
+  var columnSize = 101;
+  var rowSize = 83;
+  if (keypress === 'left' && this.x - columnSize >= 0) {
+    this.x -= columnSize;
+    // keep player move inside the canvas
+  } else if (keypress === 'right' && this.x + columnSize < ctx.canvas.width) {
+    this.x += columnSize;
+    // keep player move inside the canvas
+  } else if (
+    keypress === 'down' &&
+    this.y + rowSize < ctx.canvas.height - 200
+  ) {
+    this.y += rowSize;
+    // keep player move inside the canvas
+  } else if (keypress === 'up' && this.y - rowSize > 0 - player.height) {
+    this.y -= rowSize;
+  }
 };
 
-// Player resetGame position.
+// Player reset position.
 Player.prototype.resetGame = function() {
-    this.x = 202;
-    this.y = 404;
+  this.x = 202;
+  this.y = 400;
 };
 
 // Now instantiate Enemy objects.
@@ -101,17 +92,17 @@ var enemyThree = new Enemy(0, 226);
 var allEnemies = [enemyOne, enemyTwo, enemyThree];
 
 // Place the player object in a variable called player
-var player = new Player();
+var player = new Player(202, 400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
